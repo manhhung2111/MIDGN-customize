019eeaaf71f79e4cfa21f7d0d5c1a2461b90c5fb
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2023-11-09 17:32:22 +0700

    Attempt #2 to get best epoch

--- main.py
+++ main.py
@@ -132,7 +132,10 @@ def main():
                     if epoch % CONFIG['test_interval'] == 0:
                         output_metrics = test(model, eval_loader, device, CONFIG, metrics)
                         test_metrics = test(model, test_loader, device, CONFIG, test_metrics)
-                        print(f"Test metrics: {test_metrics}")
+                        # print(f"Test metrics: {test_metrics}")
+                        for metric in test_metrics:
+                            print(f"Test metric {metric.get_title}: {metric.metric}")
+
                         for metric in output_metrics:
                             eval_writer.add_scalars('metric/all', {metric.get_title(): metric.metric}, epoch)
                             if metric==output_metrics[0]:
